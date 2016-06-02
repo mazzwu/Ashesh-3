f7e53167ebb9970a5d0e456383b0880d3f21d6fd
vivianlikeyeah <vivian.bakiris@hotmail.com>
2016-06-02 17:29:10 +1000

    Add files via upload

--- bst.c
+++ bst.c
@@ -1,4 +1,4 @@
-// bst.c ... client for BSTree ADT
+// InvertedIndex.c
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,132 +6,160 @@
 #include <strings.h>
 #include "BSTree.h"
 #include "DLList.h"
+#include "Inverted.h"
 
-//#define MAXVALS 1000
 
 #define GREEN "\x1b[1;32m"
 #define BROWN "\x1b[0;33m"
 #define RESET "\x1b[0m"
 
-//char * makearray(FILE *f, int N, char * alan[]);
+
+DLList CollectionToList(void);
+int LengthOfCollection(DLList);
+DLList AllWordsToList(int,DLList, DLList);
 
 int main(int argc, char *argv[])
 {
 
 	BSTree Q; 
-	DLList D; 
-	DLList All;
+	DLList listOfCollection; 
+	DLList AllWords;
 	BSTree *ptr;
-	FILE *God;
+	//FILE *God;
 	DLList WithoutTxt;
 	ptr = malloc(sizeof(BSTree) * 1000);
 
 	char * txt = ".txt";
 	FILE *f;
-	char* url = NULL; 
-	char * helplease = NULL; 
+	char* urltx = NULL; 
+	char * url = NULL; 
 	char * URLname;
-	char * Jesus = NULL;
+	//char * Jesus = NULL;
+	int N;
 
 	// Open Each URL in the collection and store it in 
-	// a linked list, Also fine\d N of URLS in collection. 
-	D =newDLList();
-	FILE *collection = fopen("collection.txt", "r");
-	D= FileAppendDLList(collection,D); 
-	printf(BROWN"Printing the list of the collection\n"RESET);
-	//printList(D);
-	int N = Length(D);
-	//printList(D);
-	fclose(collection);
+	// a linked list, Also find N of URLS in collection. 
+	//D =newDLList();
+	//FILE *collection = fopen("collection.txt", "r");
+	//D= FileAppendDLList(collection,D); 
+	//printf(BROWN"Printing the list of the collection\n"RESET);
+	//int N = Length(D);
+	//fclose(collection);
+	//printf("Is it doing anything?\n");
+	//printf("Testing if function works");
+	
+	listOfCollection = CollectionToList(); 
+	N = Length(listOfCollection);
+	//printList(listOfCollection);
+
+
 
 
 	// Returns all the URLS in the linked list
 	// Adds .txt - opens it 
 	// and creates an array of BSTS of all the word. 
 	for(int i = 0; i < N; i++) {
-	helplease = help(D,i);
-	url = strcat(helplease,txt);
-	f = fopen(url, "r");
+	url = valueReturn(listOfCollection,i);
+	urltx = strcat(url,txt);
+	f = fopen(urltx, "r");
 	Q = getBSTree(f);
 	ptr[i]= Q; 
 	fclose(f);
 	}
 
+	//printf("Showing bst \n");
+
+	// This function prints all the bsts. 
+	/*
+	for( int bst = 0; bst < N; bst ++ ){
+		showBSTree(ptr[bst]);
+		//printf("new tree\n");
+	} */
+
+	
+
+	// This is a comparison linked list so that you don't add urls to your 
+	// all list 
 	// Create a second Linked list of all the URLS in the collection
 	// That don't have the .txt 
-	WithoutTxt=newDLList();
-	FILE *collection2 = fopen("collection.txt", "r");
-	WithoutTxt= FileAppendDLList(collection2,WithoutTxt); 
-	printf(BROWN"Printing the list of the collection\n"RESET);
-	printList(WithoutTxt);
-	fclose(collection);
+	// This is the same function as above
+	
+	WithoutTxt = CollectionToList();
+	//WithoutTxt=newDLList();
+	//FILE *collection2 = fopen("collection.txt", "r");
+	//WithoutTxt= FileAppendDLList(collection2,WithoutTxt); 
+	//printf(BROWN"Printing the list of the collection\n"RESET);
+	//printList(WithoutTxt);
+	//fclose(collection2);
+	//printList(listOfCollection);
+	//printList(WithoutTxt);
 
 	
 
 	//The following returns all the
 	//words in all the urls
 	// Creates a linked list of all the words
-	// 
-	All = newDLList();
-	//Alpha = newDLList();
-	// Takes in linked list D,
-	printf(BROWN"Just checking this\n"RESET);
-	printList(D);
-	for (int hope = 0; hope < N; hope++){
-		Jesus = help(D,hope);
-		//printf("The value of Jesus is %s\n", Jesus);
-		God = fopen(Jesus, "r");
-		//printList(D);
-		All = AlphaAppend(God,All,WithoutTxt);
-		
-		//Alpha =AlphaAppend(God,Alpha,WithoutTxt);;
-
-	}  
-
-	printf("Finishes All\n");
 	
-	//Alpha=FileAppendDLList3(God,All,WithoutTxt);;
-	//printf("Testing if it works\n");
-	printf("PRINTING ORIGINAL\n");
+	// Takes in linked list D,
+	//printf(BROWN"Just checking this\n"RESET);
+	//printList(listOfCollection);
+
+
+	AllWords = AllWordsToList(N,listOfCollection,WithoutTxt);
+	//AllWords = newDLList();
+	//for (int hope = 0; hope < N; hope++){
+		//Jesus = valueReturn(listOfCollection,hope);
+		//God = fopen(Jesus, "r");	
+		//AllWords = AlphaAppend(God,AllWords,WithoutTxt);
+		
+	//}  
+
+	// The following does the same as above just with a better function 
+
+	//DLList testList;
+	//testList = newDLList();
+	//char *returntext = NULL;
+	//FILE *urlOpen;
+
+	//for (int jk = 0; jk < N; jk++){
+		//printf("Does it do this?\n");
+		//returntext = valueReturn(listOfCollection,jk);
+		//urlOpen = fopen(returntext, "r");	
+		//testList = PureAppend(urlOpen,testList,WithoutTxt);
+		
+//	}  
+
+	//printf(GREEN"PRINTING NEW APPEND\n"RESET);
+	//printList(testList);
+
+
+
+
+
+	 
 	//printList(All);
 
-	printf("Try sorting now\n");
-	/*char *urlname;
-
-	for (int iteration = 0; iteration < N; iteration++){
-		urlname = help(D,iteration);
-		printf("The value of the urlname is %s\n", Jesus);
-		God = fopen(urlname, "r");
-		Alpha =AlphaAppend(God,Alpha,WithoutTxt);;
-		printf("Is it running there?\n");
-	}  
-	printf("Supposed to be sorted\n");
-	printList(Alpha);
-	*/ 
-
-	// Then we need to take in each as a search term
-
-
-	
+	// This is where you get the inverted index 
 	
 	char * searchTerm; 
 	searchTerm = (char *)malloc(3);
 	int found;
-	int numberofWords = Length(All)- 5; 
-	FILE *Inverted = fopen("In.txt","w");
+	int numberofWords = Length(AllWords); 
+	//printf("The number of words is %d\n",numberofWords);
+	FILE *Inverted = fopen("Inverted.txt","w");
 
 	
 	for (int test = 0; test <= numberofWords; test++){
 
-		searchTerm= help(All, test);
-		fprintf(Inverted, "\n%s",searchTerm);
+		searchTerm= valueReturn(AllWords, test);
+		fprintf(Inverted, "%s",searchTerm);
 
 		for(int iterator = 0; iterator <= N; iterator++){	
 		// Set the search term as the term returned by ALL	
 		found = BSTreeFind(ptr[iterator],searchTerm);
 
 		if(found == 1){
-			URLname = help(WithoutTxt, iterator);
+			URLname = valueReturn(WithoutTxt, iterator);
 			fprintf(Inverted," %s", URLname);
 		}
 		else{
@@ -140,76 +168,39 @@ int main(int argc, char *argv[])
 	
    		}
    
-   		printf("\n");
+   		fprintf(Inverted,"\n");
 
  	}
 
 
-
-	//while(fscanf(f,"%s", string) !=EOF){
-	//	printf("%s \n", string);
-	//} 
-	 /* for (int i = 1; i<N; i++){
-		// Maybe make a get l->current data function 
-		// you need to open each URL file one at a time 
-	 	printf("why\n");
-	 	strcpy(fuckyou, help(D,i));
-	 	printf(" the string is %s\n", fuckyou);
-	 	printf("i hate you \n");
-		urlTxt = strcat(fuckyou, txt); 
-		printf("this is weird");
-		f = fopen(urlTxt, "r");
-		T = getBSTree(f);
-	} 
-
-*/ 
-
-
-
-
-	//FILE *urlFILE = fopen("url11.txt", "r");
-	//T = getBSTree(urlFILE);
-	//showBSTree(T);
-	//treeTraverse(T);
-	
-
-	//fclose(urlFILE);
-
-	/* 
-
-	f = fopen("url11.txt", "r");
-	
-    char * alan[N];
-    //char * lols = NULL;
-  
-    alan[N]=makearray(f,N,alan);
-
-
-    for (int q = 0; q<= N; q++){
-    	printf("The value in the array is %s\n",alan[q]);
-    }
-	*/
-
-	/* printf("Please type maybe?\n");
-	int yo;
-	scanf("%d", &yo);
-	printf("Thanks! for typing in %d \n", yo); */ 
 	return EXIT_SUCCESS;
 }
-/* 
-char * makearray(FILE *f, int N, char * alan[]){
 
-	
-	char lols[12323];
-	for (int i = 0; i<N; i++){
-		alan[i]= malloc(1000*sizeof(char));
-		fscanf(f, "%s", lols);
-		printf("%s\n", lols);
-		//printf(" in d function %s\n", lols);
 
-	}
 
-	return *(alan);
+
+DLList CollectionToList(void){
+	DLList D;
+	D =newDLList();
+	FILE *collection = fopen("collection.txt", "r");
+	D= FileAppendDLList(collection,D);
+	fclose(collection);
+	return D;
 }
 
-*/ 
+int LengthOfCollection(DLList D){
+	int N = Length(D);
+	return N;
+}
+
+DLList AllWordsToList(int N,DLList listOfCollection, DLList WithoutTxt){
+	DLList AllWords;
+	AllWords = newDLList();
+	for (int hope = 0; hope < N; hope++){
+		char *UrlWithT = valueReturn(listOfCollection,hope);
+		FILE *urlFile = fopen(UrlWithT, "r");	
+		AllWords = PureAppend(urlFile,AllWords,WithoutTxt);	
+	}  
+
+	return AllWords;
+}
